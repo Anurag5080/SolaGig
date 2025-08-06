7be2d646dc5d1a167e8421c59c3f7f159ae4a665
Anurag Singh <anurag26official@gmail.com>
2025-08-06 11:17:13 +0530

    Update middleware.ts

--- backend/src/middleware.ts
+++ backend/src/middleware.ts
@@ -30,34 +30,6 @@ export function authMiddleware(req:Request, res: Response, next: NextFunction){
     }
 };
 
-
-
-// export function workerMiddleware(req:Request, res: Response, next: NextFunction){
-
-//     const authHeader = req.headers["authorization"] ?? "";
-
-//     try{
-//        const decode = jwt.verify(authHeader, WORKER_JWT_SECRET) as {userId: string};
-       
-//        if(decode.userId){
-//         // @ts-ignore
-//         req.userId = decode.userId;
-//         return next();
-        
-//        }else{
-//         return res.status(403).json({
-//             message: "You are not authorized to access this route"
-//         })
-//        }
-        
-//     }catch(e){
-//         return res.status(403).json({
-//             message: "Invalid token"
-//         })
-    
-//     }
-// };
-
 export function workerMiddleware(req: Request, res: Response, next: NextFunction) {
     const authHeader = req.headers["authorization"] ?? "";
     const token = authHeader.startsWith("Bearer ") ? authHeader.slice(7) : authHeader; // Remove Bearer prefix if present
@@ -78,4 +50,5 @@ export function workerMiddleware(req: Request, res: Response, next: NextFunction
             message: "Invalid token"
         });
     }
+
 }
